e5ab6cc3909010f4de23f53992e1bd6ec485e0dd
Jared Klingenberger <klinge2@g.clemson.edu>
2016-02-10 11:45:05 -0500

    Fixed logic error in seq

--- runHW1.sh
+++ runHW1.sh
@@ -10,7 +10,7 @@ fi
 mkdir -p $4
 rm -rf "$4/*"
 
-for i in `seq $2 $3 | awk '{print 2 ^ $1}'`; do
+for i in `seq $2 \`expr $2 + $3\` | awk '{print 2 ^ $1}'`; do
   echo "Running $1 iterations on blocksize $i..."
   ./hw1 $1 $i > "$4/$5.$i"
 done
